4b7bc77e46a8b08f75842bf8182aab39940611a0
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-06-29 14:59:43 +0530

    fixed country link

--- src/App.js
+++ src/App.js
@@ -60,8 +60,8 @@ const App = () => {
             {countries.map((country) => {
               const { name } = country;
               const path =
-                'REST-Countries/country/' + name.replace(/\s+/g, '-').toLowerCase();
-              return <Route path={path} key={country.alpha3Code}>
+                '/REST-Countries/' + name.replace(/\s+/g, '-').toLowerCase();
+              return <Route exact path={path} key={country.alpha3Code}>
                 <CountryPage country={country} countries={countries}></CountryPage>
               </Route>;
             })}
